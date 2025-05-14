7947324cf36e38abec1d82f09f336b759b69b953
Ben Long <155552749+SoggyWaffle27@users.noreply.github.com>
2025-05-14 14:54:21 -0400

    Update mazetest.py

--- mazetest.py
+++ mazetest.py
@@ -5,10 +5,11 @@ import sys
 # Set up the dimensions of the window and colors
 WIDTH = int(input("Dimensions: "))  # Width of the maze (in cells)
 HEIGHT = WIDTH  # Height of the maze (in cells)
-CELL_SIZE = 600 / WIDTH + 1  # Size of each cell (pixels)
+CELL_SIZE = (600 / WIDTH) + 0  # Size of each cell (pixels)
 WHITE = (255, 255, 255)  # Color for open spaces
 BLACK = (0, 0, 0)  # Color for walls
 RED = (255, 0, 0)  # Color for the start point
+BLUE = (126, 209, 246)
 
 def generate_maze(width, height):
     # Create grid initialized with walls
@@ -67,6 +68,8 @@ def display_maze(maze):
                     color = BLACK
                 elif maze[y][x] == ' ':  # Empty space
                     color = WHITE
+                elif maze[y][x] == '-':  # Empty space
+                    color = BLUE
                 if (x, y) == (1, 1):  # Start point (for visualization)
                     color = RED
                 
